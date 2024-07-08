c8ad300740c6c1b1ee85d3a7913afd60ae9c6ea4
wspellman <wspellman@loomissayles.com>
2024-07-08 18:21:43 -0400

    added mongo functionality

--- src/app.js
+++ src/app.js
@@ -1,12 +1,41 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+dotenv.config();
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+mongoose.set('strictQuery', false);
+const PORT = process.env.PORT || 3000;
+const CONNECTION = process.env.CONNECTION;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('Hello World!');
 
+app.get('/', (req, res)=> {
+    res.send("Hello world.");
+})
+
+app.post('/', (req, res)=> {
+    res.send('post request');
+})
+
+app.post('/newexpense', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
 });
 
-server.listen(3000, '127.0.0.1', () => {
-    console.log('Server running...');
-});
+const start = async() => {
+    try {
+        await mongoose.connect(CONNECTION);
+
+        app.listen(PORT, () => {
+            console.log('App is listening on port ' + PORT);
+        });
+    }
+    catch(error) {
+        console.log(error.message);
+    }
+};
+
+start();
+
+
